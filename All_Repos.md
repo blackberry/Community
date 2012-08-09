b91acf4e50cf8d00399e05417355bb8d7ed61304
Eduardo Pelegri-Llopart <pelegri@calterra.com>
2012-08-09 11:45:30 -0700

    Completed first version of All_Repos catalog.

--- All_Repos.md
+++ All_Repos.md
@@ -8,6 +8,24 @@ jsfile: All_Repos.js
 {% include common-defs.md %}
 
 This is a sortable table with all the repositories in our [GitHub Organization](http://github.com/blackberry),
-generated from a backing [JSON file](All_Repos.json).
+generated from a backing [JSON file](All_Repos.json). Also see the list of [All Samples].
 
-Also see the list of [All Samples].
+The table can be sorted (ascending/descending) on a sequence of one or more columns.  To select additional
+columns select the next column while pressing the 'SHIFT' (on mac) key. The table uses these tooltips:
+<span class="question" tip="(Repo column) A list of 'tags' characterizing this repo (useful?)">T</span>, 
+<span class="question" tip="(Repo column) Extra information on the branches in the repository">B</span>,
+<span class="question" tip="(Repo column) Repo has Pages">P</span>,
+<span class="question" tip="(Repo column) Repo has a Wiki">W</span>,
+<span class="question" tip="(Desc column) Extra details on the repository">?</span>,
+<span class="warning" tip="(Desc column) Issues to resolve">!</span>,
+<span class="question" tip="(in BB10 column) Ported to PlayBook but not yet tested on BB10">?</span>,
+<span class="question" tip="(in Port column) Extra information on porting">P</span>.
+
+_*Notes*_: We are interested in your feedback on how to best present this information.  In particular:
+
+*Q1*: Is the list of tags useful, or just extra noise.
+
+*Q2*: Should we split the table into several tables?  Perhaps one for each platform, plus one for all ports?
+That way each table would then be smaller and with fewer columns.
+
+*N3*: We may have to start tracking non-RIM repos (e.g. opendataspace clients).
--- js/All_Repos.js
+++ js/All_Repos.js
@@ -2,15 +2,18 @@ $(document).ready(function(){
     $.getJSON('/Community/All_Repos.json', function(data) {
 	var items = [];
 	
+	/* total hack on the overlap; need to fix */
 	items.push('<thead>' +
 		   '<tr>' +
 		   '<th>Repository</th>' + 
 		   '<th>Description</th>' +
-		   '<th>Platform</th>' +
+		   '<th>Type</th>' +
 		   '<th>BB10</th>' +
 		   '<th>Sample</th>' +
 		   '<th>Port</th>' +
-		   '<th>Tags</th>' +
+/* REMOVED for now
+		   '<th>Tags&nbsp;&nbsp;&nbsp;</th>' +
+*/
 		   '</tr>' +
 		   '</thead>')
 
@@ -18,20 +21,31 @@ $(document).ready(function(){
 	    items.push('<tr>' +
 
 		       /* Repository */
-		       '<td><a href="' + val.url + '" target="_blank">' + key + '</a>' +
-		       ( ( val.branches && val.branches.length > 1 )
-			 ? ' <span class="question" tip="' +
+		       '<td><span style="white-space: nowrap;"><a href="' + val.url + '" target="_blank">' + key + '</a></span>' +
 
-			 val.branches +
+		       ' <span style="white-space: nowrap;">'+
+		          ( ( (val.branches && val.branches.length > 1 ) || (val.branchinfo))
+			    ? ' <span class="question" tip="' +  val.branches +
+			    ( ( val.branchinfo )
+			      ? val.branchinfo
+			      : ''
+			    ) +
+			    '">B</span>'
+			    : ''
+			  ) +
+		          ( ( $.inArray("wiki", val.tags) >= 0 )
+			    ? ' <span class="question" tip="Repo has a Wiki">W</span>'
+			    : ''
+			  ) +
+		          ( ( $.inArray("pages", val.tags) >= 0 )
+			    ? ' <span class="question" tip="Repo has a Pages local site">P</span>'
+			    : ''
+			  ) +
+		          ( ( val.tags )
+			    ? ' <a href="' + val.url + '#readme"><span class="question" tip="' + val.tags + '">T</span></a>'
+			    : '' ) +
+		       '</span>' +
 
-			 ( ( val.branchinfo )
-			   ? "<p>" + val.branchinfo + "</p>"
-			   : ''
-			 ) +
-
-			 '">B</span>'
-			 : ''
-		       ) +
 		       '</td>' +
 
 		       /* Description */
@@ -40,30 +54,43 @@ $(document).ready(function(){
 			    ? ' <span class="question" tip="' + val.note + '">?</span>'
 			    : ''
 			  ) +
+		          ( ( val.warning )
+			    ? ' <span class="warning" tip="' + val.warning + '">!</span>'
+			    : ''
+			  ) +
 		       '</td>' +
 
 		       /* Platform */
-		       '<td>' + val.type + '</td>' +
+		       '<td class="centered">' + val.type + '</td>' +
 
 		       /* BB10 */
-		       '<td>' + ( ($.inArray("bb10", val.tags) >= 0) ? "&check;" : "") + '</td>' +
+		       '<td class="centered">' + (
+			   ($.inArray("bb10", val.tags) >= 0)
+			       ? '&check;'
+			       :  ( ($.inArray("playbook", val.tags) >= 0)
+				    ? '<span class="question" left="yes" tip="Tested on PlayBook, not yet on BB10">?</span>'
+				    : '')
+			       ) +
+		       '</td>' +
 
 		       /* Samples */
-		       '<td>' + ( ($.inArray("samples", val.tags) >= 0) ? "&check;" : "") + '</td>' +
+		       '<td class="centered">' + ( ($.inArray("samples", val.tags) >= 0) ? "&check;" : "") + '</td>' +
 
 		       /* Ports */
-		       '<td>' + ( ($.inArray("port", val.tags) >= 0) ? "&check;" : "") +
+		       '<td class="centered">' + ( ($.inArray("port", val.tags) >= 0) ? "&check;" : "") +
 		       ( ( val.portinfo)
 			 ? '<span class="question" left="yes" tip="' + val.portinfo + '">P</span>'
 			 : '' ) +
 		       '</td>' +
 
 		       /* Tags */
-		       '<td>' +
+		       /* REMOVED for now
+		       '<td class="centered">' +
 		       ( ( val.tags )
 			 ? '<a href="' + val.url + '#readme"><span class="question" left="yes" tip="' + val.tags + '">T</span></a>'
 			 : '' ) +
 		       '</td>' +
+		       */
 
 		       '</tr>');
 	});
@@ -73,14 +100,48 @@ $(document).ready(function(){
 	    html: items.join('')
 	}).appendTo('#contentbody').addClass("tablesorter");
 
+
+	// add new widget called repeatHeaders 
+	$.tablesorter.addWidget({ 
+	    // give the widget a id 
+	    id: "repeatHeaders", 
+	    // format is called when the on init and when a sorting has finished 
+	    format: function(table) { 
+		// cache and collect all TH headers 
+		if(!this.headers) { 
+		    var h = this.headers = [];  
+		    $("thead th",table).each(function() { 
+			h.push( 
+			    "<th>" + $(this).text() + "</th>" 
+			); 
+			
+		    }); 
+		} 
+		
+		// remove appended headers by classname. 
+		$("tr.repeated-header",table).remove(); 
+		
+		// loop all tr elements and insert a copy of the "headers"     
+		for(var i=0; i < table.tBodies[0].rows.length; i++) { 
+		    // insert a copy of the table head every 10th row 
+		    if((i%20) == 19) { 
+			$("tbody tr:eq(" + i + ")",table).before( 
+			    $("<tr></tr>").addClass("repeated-header").html(this.headers.join(""))
+			    
+			);     
+		    } 
+		} 
+	    } 
+	}); 
+
 	/* Sort the table */
 	$("#allrepos").tablesorter({
 	    // Stripping looking
-	    widgets: ['zebra']
+	    widgets: ['zebra', 'repeatHeaders']
 	});
 
 	/* Add the tooltips */
-	$("span.question").hover(function() {
+	$("span.question, span.warning").hover(function() {
 	    if ($(this).attr("left")=="yes") {
 		$(this).append('<div class="tooltipLeft"><p>' +
 			       $(this).attr("tip") +
